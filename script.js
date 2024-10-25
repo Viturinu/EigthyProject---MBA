956b705ba375d0b70c12322d9cf49551ff472313
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-10-25 18:06:35 -0300

    percorrendo itens da lista

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+const expenseList = document.querySelector("ul");
+const expensesQuantity = document.querySelector("aside header p span");
 
 amount.addEventListener("input", (event)=>{
  let value = amount.value.replace(/\D/g,"");
@@ -17,4 +22,73 @@ function formatCurrencyBRL(value){
     })
 
     return value;
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date()
+    }
+
+    expenseAdd(newExpense);
+}
+
+function expenseAdd(newExpense){
+    try{
+        const expenseList = document.querySelector("ul");
+
+        const expenseItem = document.createElement("li");
+        expenseItem.classList.add("expense");
+
+        const expenseIcon = document.createElement("img");
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+        expenseIcon.setAttribute("alt", newExpense.category_name);
+
+        const expenseInfo = document.createElement("div");
+        expenseInfo.classList.add("expense-info");
+
+        const expenseName = document.createElement("strong");
+        expenseName.textContent = newExpense.expense;
+
+        const expenseCategory = document.createElement("span");
+        expenseCategory.textContent = newExpense.category_name;
+
+        expenseInfo.append(expenseName, expenseCategory);
+
+        const expenseAmount = document.createElement("span");
+        expenseAmount.classList.add("expense-amount");
+        expenseAmount.innerHTML = `<small>R$</small> ${newExpense.amount.toUpperCase().replace("R$", "")}`;
+
+        const removeIcon = document.createElement("img");
+        removeIcon.classList.add("remove-icon");
+        removeIcon.setAttribute("src", "img/remove.svg");
+        removeIcon.setAttribute("alt", "remover")
+        removeIcon.addEventListener("click", ()=>{
+            throw new Error("Testando objeto de Error");
+        })
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
+
+        expenseList.append(expenseItem);
+
+        updateTotals();
+    } catch(error){
+        alert("Não foi possível atualizar a lista de despesas.")
+    }
+}
+
+function updateTotals(){
+    try {
+        const items = expenseList.children;
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+    } catch (error) {
+        console.log(error);
+        alert("Não foi possível atualizar os totais.")
+    }
 }
